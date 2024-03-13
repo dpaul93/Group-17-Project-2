074e510d0998ad0290b7637831f056e68813ca62
Matt Dunmore <mdunmore89@gmail.com>
2024-03-13 22:26:06 +0000

    added more spacing beneath h1

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,7 @@ function Home() {
         <div className="header container-fluid">
             <div className="d-flex justify-content-center align-items-center h-100">
                 <div className="row back py-5">
-                    <h1 className="col-12 display-1 fw-bold dark text-center"> Yappa</h1>
+                    <h1 className="col-12 display-1 fw-bold dark text-center mb-3"> Yappa</h1>
                     <h5 className="col-12 dark text-center">Instant messaging for chatty people</h5>
                     <div className="col-12 d-flex justify-content-center mt-3">
                         <GoogleSignInButton />
