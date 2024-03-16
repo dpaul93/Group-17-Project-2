2016218ed12a1b96df228946a04023c0d1edcc27
Sarenne89 <mdunmore89@gmail.com>
2024-03-16 11:45:43 +0000

    added buttons to logout or move to chat

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect} from 'react';
+import { Link } from 'react-router-dom';
 import "../styles/Home.css";
 import axios from 'axios'
 import {useGoogleLogin, googleLogout} from '@react-oauth/google'
-import GoogleSignInButton from './googleLogin';
 
 function Home() {
 
@@ -43,7 +43,7 @@ return (
     <div className="header container-fluid">
         {profile ? (
             <div className="d-flex justify-content-center align-items-center h-100">
-                <div className="row back py-5 text-center">
+                <div className="row back py-5 text-center justify-content-center">
                     <h1 className="col-12 display-1 fw-bold dark text-center mb-3"> Yappa</h1>
                     <div classname="col-3 d-flex justify-content-center">
                         <img className="img-fluid" src={profile.picture} alt="user image" />
@@ -51,23 +51,30 @@ return (
                     <h3>User Logged in</h3>
                     <p>Name: {profile.name}</p>
                     <p>Email Address: {profile.email}</p>
-                    <div>
-                        <button className="btn btn-primary col-3" onClick={logOut}>Log out</button>
+                    <div className="row col-6 justify-content-between">
+                        <div className=" mx-auto col-4">
+                            <Link to="/Chat">
+                                <button className="col-12 btn btn-primary">Enter the chat!</button>
+                            </Link>
+                        </div>
+                        <div className="mx-auto col-4">
+                        <   button className="col-12 btn btn-danger" onClick={logOut}>Log out</button>
+                        </div>
                     </div>
                 </div>
     </div>
     ) : (
     <div className="header container-fluid">
-    <div className="d-flex justify-content-center align-items-center h-100">
-        <div className="row back py-5">
-            <h1 className="col-12 display-1 fw-bold dark text-center mb-3"> Yappa</h1>
-            <h5 className="col-12 dark text-center">Instant messaging for chatty people</h5>
-            <div className="col-12 d-flex justify-content-center mt-3">
-            <button className="btn btn-primary" onClick={() => login()}>Sign in with Google</button>
+        <div className="d-flex justify-content-center align-items-center h-100">
+            <div className="row back py-5">
+                <h1 className="col-12 display-1 fw-bold dark text-center mb-3"> Yappa</h1>
+                <h5 className="col-12 dark text-center">Instant messaging for chatty people</h5>
+                <div className="col-12 d-flex justify-content-center mt-3">
+                <button className="btn btn-primary" onClick={() => login()}>Sign in with Google</button>
+                </div>
             </div>
         </div>
     </div>
-</div>
 )}
     </div>
 
