ecb1b5acfa00ba57b41a006a40d516e3e9db5c8b
Dominic <dominicepaul@gmail.com>
2024-03-20 00:37:56 +0000

    home.jsx edit to intergrate socket.io within the google api so it functions with our chat app

--- Server/index.js
+++ Server/index.js
@@ -5,7 +5,7 @@ const http = require('http').Server(app);
 const PORT = 4000
 const socketIO = require('socket.io')(http, {
     cors: {
-        origin: "http://localhost:3000"
+        origin: "http://localhost:5174"
     }
 });
 
--- src/components/chat-bar/ChatBar.jsx
+++ src/components/chat-bar/ChatBar.jsx
@@ -25,10 +25,11 @@ import React, {useState, useEffect} from 'react'
 const ChatBar = ({socket}) => {
     const [users, setUsers] = useState([])
 
-    useEffect(()=> {
-        socket.on("newUserResponse", data => setUsers(data))
-    }, [socket, users])
+    socket.on("newUser", data => setUsers(data))
 
+    useEffect(()=> {
+        // socket.on("newUser", data => setUsers(data))
+    }, [socket, users])
   return (
     <div className='chat__sidebar'>
         <h2>Open Chat</h2>
