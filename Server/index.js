5b50799647ff23b4406da248b4ddb310f929cc2a
Dominic <dominicepaul@gmail.com>
2024-03-21 11:07:17 +0000

    added path to server

--- Server/index.js
+++ Server/index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const app = express()
 const cors = require("cors")
 const http = require('http').Server(app)
+const path = require('path')
 // const PORT = process.env.PORT || 3000
 const socketIO = require('socket.io')(http, {
     cors: {
